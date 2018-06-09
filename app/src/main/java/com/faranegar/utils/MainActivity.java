7b40a51e587deba6e09146f92c8d948354cb4478
shahabgolpaiegandev <shahabgolpaiegan@gmail.com>
2018-06-09 11:49:52 +0430

    made FaranegarUtils static

--- app/src/main/java/com/faranegar/utils/MainActivity.java
+++ app/src/main/java/com/faranegar/utils/MainActivity.java
@@ -3,6 +3,8 @@ package com.faranegar.utils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.faranegar.faranegarutils.FaranegarUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
